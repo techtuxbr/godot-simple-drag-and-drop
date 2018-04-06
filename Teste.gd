f80d08c511393cc2f07802aa6d4d55e90eba1742
Victor Lima <jvlpark3@outlook.com>
2018-04-05 23:04:08 -0300

    Update Teste.gd

--- Teste.gd
+++ Teste.gd
@@ -8,6 +8,8 @@ func _ready():
 	pass
 
 func _process(delta):
+	# You need to create a action caleld "click"(Left mouse click) in the project input map.
+	# Você precisa criar uma ação chamada click(botão esquerdo do mouse) no input map do seu projeto
 	if(mouseIn && Input.is_action_pressed("click")):
 		set_position(get_viewport().get_mouse_position())
 	pass
